ee165567710364a0406993beee8ae58ab5ef7290
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-02-15 18:10:10 +0600

    complete pin matcher wrong or right message task and resolve some error issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/* 
+-----------Pin Generator Code here----------
+*/
+
 function getPin() {
     const pin = generatePin();
     const pinString = pin + '';
@@ -10,35 +14,62 @@ function getPin() {
 
 }
 function generatePin() {
-    const random = Math.round(Math.random()*100000);
+    const random = Math.round(Math.random() * 100000);
     return random;
 }
 
-document.getElementById('generate-pin').addEventListener('click', function(){
+document.getElementById('generate-pin').addEventListener('click', function () {
     const pin = getPin();
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
 })
 
+/* 
+-----------Calculator Field and Number button Code here----------
+*/
 
-document.getElementById('calculator').addEventListener('click',function(event){
+document.getElementById('calculator').addEventListener('click', function (event) {
     const calculatorValue = event.target.innerText;
     const typedNumberField = document.getElementById('typed-number');
     const previousTypedNumber = typedNumberField.value;
-    if(isNaN(calculatorValue)){
-        if(calculatorValue === 'C'){
+    if (isNaN(calculatorValue)) {
+        if (calculatorValue === 'C') {
             typedNumberField.value = '';
         }
-        else if(calculatorValue === "<"){
+        else if (calculatorValue === "<") {
             const digits = previousTypedNumber.split('');
             digits.pop();
             const remainDigits = digits.join('');
-            typedNumberField.value = remainDigits; 
-            
+            typedNumberField.value = remainDigits;
+
         }
     }
-    else{
+    else {
         const newTypedNumber = previousTypedNumber + calculatorValue;
         typedNumberField.value = newTypedNumber;
     }
 })
+
+/* 
+-----------Pin Matcher Message Code here----------
+*/
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-number');
+    const typedNumber = typedNumberField.value;
+
+    const rightPinText = document.getElementById('right-pin');
+    const wrongPinText = document.getElementById('wrong-pin');
+    if (typedNumber === currentPin) {
+        wrongPinText.style.display = 'none';
+        rightPinText.style.display = 'block';
+    }
+    else {
+        rightPinText.style.display = 'none';
+        wrongPinText.style.display = 'block';
+    }
+})
+// -------------------Complete-Project--------------//
