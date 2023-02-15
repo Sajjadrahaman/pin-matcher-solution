dacee82ccc00f475b309b1afd93b84a4325a7f77
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-02-15 16:42:15 +0600

    develop calculator number Typed Field for matching pin and develop calculator clear and remove button by using Dom

--- js/app.js
+++ js/app.js
@@ -19,3 +19,26 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
 })
+
+
+document.getElementById('calculator').addEventListener('click',function(event){
+    const calculatorValue = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-number');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(calculatorValue)){
+        if(calculatorValue === 'C'){
+            typedNumberField.value = '';
+        }
+        else if(calculatorValue === "<"){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainDigits = digits.join('');
+            typedNumberField.value = remainDigits; 
+            
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + calculatorValue;
+        typedNumberField.value = newTypedNumber;
+    }
+})
